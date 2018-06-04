2ff0eb128fdf0e976ada16d91180a1aafae217f5
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-06-03 20:29:49 -0500

    added overloaded method that returns the result of the other method if valid

--- src/com/ianl/Main.java
+++ src/com/ianl/Main.java
@@ -3,17 +3,33 @@ package com.ianl;
 public class Main {
 
     public static void main(String[] args) {
-        calcFeetAndInchesToCentimeters(0, 12); // pass
-        calcFeetAndInchesToCentimeters(-1, 12); // fail
-        calcFeetAndInchesToCentimeters(0, 13); // fail
-        calcFeetAndInchesToCentimeters(24, 12); // pass
+//        System.out.println(calcFeetAndInchesToCentimeters(0, 12)); // pass
+//        System.out.println(calcFeetAndInchesToCentimeters(-1, 12)); // fail
+//        System.out.println(calcFeetAndInchesToCentimeters(0, 13)); // fail
+//        System.out.println(calcFeetAndInchesToCentimeters(24, 12)); // pass
+
+        System.out.println(calcFeetAndInchesToCentimeters(12));
+        System.out.println(calcFeetAndInchesToCentimeters(76));
+        System.out.println(calcFeetAndInchesToCentimeters(1289));
+        System.out.println(calcFeetAndInchesToCentimeters(-24));
     }
 
-    public static int calcFeetAndInchesToCentimeters(double feet, double inches) {
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
         if ((feet >= 0) && (inches >= 0 && inches <=12)) {
-            System.out.println("Validated with: "+feet+" ft "+inches+" in ");
-            return 0;
+//            System.out.println("Validated with: "+feet+"ft "+inches+"in ");
+            return ((feet * 12d) + (inches))*(2.54d);
         }
+        return -1;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if(inches >= 0) {
+            double feet = (inches/12);
+            double remainingInches = (inches % 12);
+            System.out.println("Valid with: "+feet+"ft "+remainingInches+"in");
+            return calcFeetAndInchesToCentimeters(feet, remainingInches);
+        }
+        System.out.println("Invalid");
         return -1;
     }
 }
