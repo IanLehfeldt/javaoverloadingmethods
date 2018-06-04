234a1cc293f799a9a90a19613afcf91ede5db110
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-06-03 20:04:24 -0500

    creating an overloaded method and calling one method in another

---
+++ src/com/ianl/Main.java
@@ -0,0 +1,26 @@
+package com.ianl;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+// Create a method called calcFeetAndInchesToCentimeters
+// needs to take in two parameters
+// feet is the first, inches the second
+
+// validate first parameter >= 0
+// validate second to be >=0 and<=12
+// return -1 if neither is not true
+
+// if valid, calculate how many centimeters comprise the
+// feet and inches passed to the method and return that value
+
+// second method w/ the same name but only accepts inches as parameter
+// validate that inches are >= 0, return -1 if not true
+// if valid, calculate how many feet are in the inches
+// call the other method to calculate how many centimeters are in the value
+// use double as data types
+// 1 inch = 2.54cm, 1 foot = 12 inches
